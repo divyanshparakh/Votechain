f8e290949bbbbd6dfa4e67972b2870c95962a816
Divyansh Parakh <divyanshparakh11@gmail.com>
2022-04-25 17:23:37 +0530

    first part

---
+++ config.js
@@ -0,0 +1,11 @@
+const DB_USER = 'pes2ug19cs119';
+const PASSWORD = encodeURIComponent('PaSsWoRd')
+
+var config = {
+    microsoftApiKey: '06b5c355846b4eff8074a08230765499',
+    microsoftDetectURL: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect",
+    microsoftVerifyURL: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/verify',
+    mongoURI: 'mongodb+srv://pes2ug19cs119:PaSsWoRd@votechain.dpbxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
+
+module.exports = config;
